6c9e77177dc6a022115d6065311fb27d4d20e83a
Justnoones <yanw97841@gmail.com>
2023-08-23 13:55:03 +0630

    two-way-data-binding

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -4,17 +4,15 @@ import { useState } from 'react';
 
 export default function PostForm() {
     let  [title, setTitle] = useState("");
-    // let onChangeHandler = e => {
-    //     setTitle(e.target.value);
-    // };
   return (
     <form className='post-form-component'>
         <h1>Create Post</h1>
         <div className='form-control'>
             <label htmlFor=''>Title</label>
-            <input type='' name='' id='' className='' onChange={e => setTitle(e.target.value)} />
+            <input type='' name='' id='' className='' onChange={e => setTitle(e.target.value)} value={title} />
         </div>
         <p>{title}</p>
+        <button type='button' onClick={_ => setTitle('')}>Reset Form</button>
         <div className='form-control'>
             <button type='submit'>Post Now</button>
         </div>
